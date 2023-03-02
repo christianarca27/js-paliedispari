09f13cfe26b307aea20047e974db46e19c0c4c8b
christianarca27 <christian.arca.mail@gmail.com>
2023-03-02 15:18:43 +0100

    feat: implementata funzione della seconda richiesta

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const buttonAnalyzeEl = document.getElementById("btn-analyze");
-console.log(buttonAnalyzeEl);
+
+const buttonPlayEl = document.getElementById("btn-play");
+console.log(buttonPlayEl);
 
 buttonAnalyzeEl.addEventListener("click", function() {
     const wordInputEl = document.getElementById("word-input");
@@ -11,6 +13,30 @@ buttonAnalyzeEl.addEventListener("click", function() {
     }
     else {
         result1El.innerText = "La parola " + wordInput + " NON è palindroma.";
+    }
+});
+
+buttonPlayEl.addEventListener("click", function() {
+    const result2El = document.getElementById("result-2");
+
+    let userChoice = document.getElementById("even-odd-input").value;
+    console.log(userChoice);
+
+    let userNumber = parseInt(document.getElementById("number-input").value);
+    console.log(userNumber);
+
+    let pcNumber = parseInt(generateRandomNumber(1, 5));
+    console.log(pcNumber);
+    
+    result2El.innerHTML = "Hai giocato: " + userNumber + "<br>" + "Il pc ha giocato: " + pcNumber + "<br>";
+
+    if(isEvenOrOdd(userNumber + pcNumber) == userChoice) {
+        console.log("Hai vinto !");
+        result2El.innerHTML += "Hai vinto !";
+    }
+    else {
+        console.log("Hai perso..");
+        result2El.innerHTML += "Hai perso..";
     }
 });
 
@@ -36,4 +62,29 @@ function isPalindrome(word) {
     else {        
         return false;
     }
+}
+
+
+/**
+ * Restituisce la stringa "pari" se number è pari, "dispari" se è dispari.
+ * @param {number} number
+ * @returns {string}
+ */
+function isEvenOrOdd(number) {
+    if(number % 2 == 0) {
+        return "even";
+    }
+    else {
+        return "odd";
+    }
+}
+
+/**
+ * Genera un numero casuale compreso tra min e max.
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
